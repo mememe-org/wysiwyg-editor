ca7a880902ad16cb2c5bc3fffaaf9b58fa97f581
moraruchan <kwanchaaaa@gmail.com>
2019-04-20 15:37:57 +0700

    server

--- server.js
+++ server.js
@@ -7,26 +7,18 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-function connect_db_user(username){
-    var databaseUrl = 'mememe';
-    var collections = [username];
-    var db = mongojs(databaseUrl, collections);
-    module.exports = {
-        connect: db
-    };
-};
-
-
+var database = 'mememe';
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 //Home
 app.get('/api/:username', (req, res) => {
-  usernamenaka=req.params.username
-  connect_db_user(usernamenaka)
+  var usernamenaka=req.params.username
+  var db = mongojs(database, [usernamenaka]);
+  module.exports = {connect: db };
    // drop database collection
-  //db.users.drop()
+  //db.usernamenaka.drop()
   db.usernamenaka.find(function(err,docs){
     console.log(docs)
     res.send(docs)
@@ -35,8 +27,8 @@ app.get('/api/:username', (req, res) => {
 
 //Create
 app.post('/api/:username',(req,res)=>{
-    usernamenaka=req.params.username
-    connect_db_user(usernamenaka)
+  var usernamenaka=req.params.username
+  var db = mongojs(database, [usernamenaka]);
     db.usernamenaka.find(function(err,docs){
     console.log(docs.length)
     var newmeme={
@@ -49,8 +41,8 @@ app.post('/api/:username',(req,res)=>{
 
 //Remove
 app.get('/api/:username/:memeid', (req, res) => {
-    usernamenaka=req.params.username
-    connect_db_user(usernamenaka)
+  var usernamenaka=req.params.username
+  var db = mongojs(database, [usernamenaka]);
     console.log("remove id: "+ObjectId(req.params.memeid))
     var mq={_id:ObjectId(req.params.memeid)}
     db.usernamenaka.remove(mq)
